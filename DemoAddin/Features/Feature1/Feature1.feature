9e369091efbc23396584824bedc307562c2e8878
Nacho <joseignacio.rodriguez@tajamar365.com>
2016-04-04 15:39:28 +0200

    no message

---
+++ DemoAddin/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="8ac66d6c-3485-4334-8087-5c507d4be984" description="SharePoint Add-in Feature" featureId="8ac66d6c-3485-4334-8087-5c507d4be984" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="DemoAddin Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c694d4d4-9aa8-4458-9f6d-cd8b20eb647a" />
+    <projectItemReference itemId="9f9c0cf2-5c0a-4405-9d77-977745c7684e" />
+    <projectItemReference itemId="76644638-6da6-4217-99ba-06cfaf8b6b6e" />
+    <projectItemReference itemId="59b2e4ae-7f6d-4ef2-84ec-4a02845dbf66" />
+    <projectItemReference itemId="534a9355-840d-427e-83e5-53aab88625fa" />
+    <projectItemReference itemId="b848e9a9-6281-4057-8868-87343e4ba3d9" />
+  </projectItems>
+</feature>
---
+++ DemoAddin/Scripts/App.js
@@ -0,0 +1,177 @@
+﻿'use strict';
+
+var Colegas = window.Colegas || {};
+
+//Colegas.App = function () {
+
+var personasList;
+var personas;
+var context;
+
+function init() {
+    context = SP.ClientContext.get_current();
+    getAllPersonas();
+}
+
+var getAllPersonas = function () {
+    personasList = context.get_web().get_lists().getByTitle("Personas");
+    personas = personasList.getItems(new SP.CamlQuery());
+    context.load(personas);
+    context.executeQueryAsync(onPersonasSuccess, onPersonasFail);
+};
+var onPersonasSuccess = function (sender, args) {
+    var html = "<ul>";
+    var personasEnum = personas.getEnumerator();
+
+    while (personasEnum.moveNext()) {
+        var actual = personasEnum.get_current();
+
+        html += "<li>" + actual.get_item("Nombre") + " " +
+            actual.get_item("Edad") + "</li>";
+    }
+    html += "</ul>";
+    $("#lista").html(html);
+};
+var onPersonasFail = function (sender, args) {
+    alert("Error:" + args.get_message());
+};
+
+var crearPersona = function () {
+    var itemCreateInfo = new SP.ListItemCreationInformation();
+    var actual = personasList.addItem(itemCreateInfo);
+    actual.set_item("Nombre", $("#input-nombre").val());
+    actual.set_item("Edad", $("#input-edad").val());
+    actual.update();
+    context.load(actual);
+    context.executeQueryAsync(onCreateSuccess, onPersonasFail);
+};
+var onCreateSuccess = function (sender, args) {
+    getAllPersonas();
+};
+/* return {
+     getPersonas: getAllPersonas,
+     createPersona: crearPersona,
+     init: init
+ }*/
+//}
+$(document).ready(function () {
+    $("#btn-add").bind("click", crearPersona);
+    ExecuteOrDelayUntilScriptLoaded(
+        function () {
+            init();
+        }
+        , "sp.js");
+});
+
+//'use strict';
+
+//var Amix = window.Personas || {}; //creo un objeto personas (no tiene nada que ver con la lista) y el objeto personas me lo recuperas del objeto windows.personas, 
+//                                     //si existe lo recupero si no existe me lo creas (esto lo hace al poner las llaves vacias).
+
+
+////Esto seria como una especie de clase
+////Amix.App = function () {
+   
+//    //objetos publicos a nivel de app
+//    var personasList;
+//    var personas;
+//    var context;
+
+//    var init = function () {//lo meto en una funcion para estar convencido que el context, se carga correctamente.
+//        context = SP.ClientContext.get_current();
+//        getAllPersonas();
+
+//    };
+
+//    var onPersonasSuccess = function (sender, args) {//sender objeto que genera la llamada al evento, args argumentos que vamos a manejar en la peticion
+//        var html = "<ul>";
+//        var personasEnum = personas.getEnumerator(); //enumeracion de todos los items
+
+//        while (personasEnum.moveNext()) {
+//            var actual = personasEnum.get_current();
+
+//            html += "<li>" + actual.get_item("Nombre") + " " + actual.get_item("edad") + "</li>";
+//        }
+//        html += "</ul>";
+//        $("#lista").html(html);
+//    };
+
+//    var onPersonasFail = function (sender, args) {
+//        alert("Error" + args.get_message());
+//    };
+
+
+//    //funcion que me devuelve todas las personas de la lista
+//    var getAllPersonas = function () {
+//        personasList = context.get_web().get_lists().getByTitle("Personas");//get_web detalles de la aplicacion web, get_lists me da el conjutno de listas de la aplicacion, 
+//                                                                           //getbytitle me das la lista personas
+//        personas = personasList.getItems(new SP.CamlQuery());
+//        context.load(personas);//le dice al contexto que meta esa peticion al context para la siguiente query.
+//        context.executeQueryAsync(onPersonasSuccess, onPersonasFail);// y ahora vete y haz la peticion a la api para devolverme los datos. si todo va bien va por el success, si no por onpersonasfail
+
+//    };
+
+   
+    
+//    var crearPersona = function () {
+//        var itemCreateInfo = new SP.ListItemCreationInformation();
+//        var actual = personasList.addItem(itemCreateInfo);
+//        actual.set_item("Nombre", $("#input-nombre").val());
+//        actual.set_item("Edad", $("#input-edad").val());
+//        actual.update();
+//        context.load(actual);
+//        context.executeQueryAsync(onCreateSuccess, onPersonasFail);
+//    };
+
+//    var onCreateSuccess = function (sender, args) {
+//        getAllPersonas();
+//    };
+
+//    //return {//devuelvo un objeto json defino los elementos privados a publicos
+//    //    //getpersonas:nombre funcion / getAllpersonas: 
+//    //    getPersonas: getAllPersonas,
+//    //    createpersona: crearPersona,
+//    //    init: init
+//    //};
+////};
+
+//$(document).ready(function () {
+//    $("#btn-add").bind("click", crearPersona);
+//    ExecuteOrDelayUntilScriptLoaded(
+//        function(){
+//           init();
+//        }
+//        , "sp.js");
+//});
+
+
+
+
+
+//function initializePage()
+//{
+//    var context = SP.ClientContext.get_current();
+//    var user = context.get_web().get_currentUser();
+
+//     This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+//    $(document).ready(function () {
+//        getUserName();
+//    });
+
+//     This function prepares, loads, and then executes a SharePoint query to get the current users information
+//    function getUserName() {
+//        context.load(user);
+//        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+//    }
+
+//     This function is executed if the above call is successful
+//     It replaces the contents of the 'message' element with the user name
+//    function onGetUserNameSuccess() {
+//        $('#message').text('Hello ' + user.get_title());
+//    }
+
+//     This function is executed if the above call fails
+//    function onGetUserNameFail(sender, args) {
+//        alert('Failed to get user name. Error:' + args.get_message());
+//    }
+//}
